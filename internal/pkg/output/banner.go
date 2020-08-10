fbb8093868c77830637c2b43ec9f2a51b9783a7a
l0ss <mike@mikeloss.net>
2020-08-10 20:10:16 +0800

    banner gud.

--- internal/pkg/output/banner.go
+++ internal/pkg/output/banner.go
@@ -6,13 +6,33 @@ import (
 	"github.com/fatih/color"
 )
 
-// TODO: banner!!!!!!!!!
+var banner = [11]string{
+	" _______ __                 ___  ___ __            ",
+	"|   _   |  |--.-----.---.-.'  _.'  _|  .-----.----.",
+	"|   1___|     |     |  _  |   _|   _|  |  -__|   _|",
+	"|____   |__|__|__|__|___._|__| |__| |__|_____|__|  ",
+	"|:  1   |              _______       __       __   ",
+	"|::.. . |             |   _   .-----|__.-----|  |_ ",
+	"`-------'             |.  1   |  _  |  |     |   _|",
+	"                      |.  ____|_____|__|__|__|____|",
+	" by @sh3r4_hax        |:  |                        ",
+	"  & @mikeloss         |::.|                        ",
+	"                      `---'                        ",
+}
 
 // Example colours
 var (
 	GoldishMaybe = color.New(color.FgHiYellow, color.Bold)
+	ReddishMaybe = color.New(color.FgRed, color.Bold)
 )
 
 func PrintBanner() {
-	fmt.Println(GoldishMaybe.Sprint("TODO: BANNER"))
+	for index, bannerline := range banner {
+		if index <= 3 {
+			fmt.Println(GoldishMaybe.Sprint(bannerline))
+		} else {
+			fmt.Print(GoldishMaybe.Sprint(bannerline[0:20]))
+			fmt.Println(ReddishMaybe.Sprint(bannerline[21:51]))
+		}
+	}
 }
